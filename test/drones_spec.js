99133d7c9df3b1c8db5e9a91162b356d15525a8e
abenedykt <spam+c9io@benedykt.net>
2015-04-20 16:13:05 +0000

    first working unit test :)

---
+++ test/drones_spec.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+
+describe('/drones', function(){
+    
+    describe('get', function(){
+    
+        it('test setup',function(){
+            expect(true).to.be.ok;
+        });
+        
+    });
+    
+});
